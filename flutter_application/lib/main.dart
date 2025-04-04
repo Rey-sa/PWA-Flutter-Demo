6ad87e1654e631080acb0bb4fdff41b675b9c4bd
rey <saskia.rey96@gmail.com>
2025-04-04 18:13:00 +0200

    Added ApiClient Object

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:openapi/api.dart';
 
 void main() {
   runApp(const MyApp());
+  var apiClient = ApiClient(basePath: 'http://localhost:8080');
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
