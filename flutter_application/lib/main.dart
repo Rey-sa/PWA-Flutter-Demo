50356f0d67f609fd0d323946d883e6a230e4edd6
rey <saskia.rey96@gmail.com>
2025-04-06 20:55:37 +0200

    Experimented with Background and Stacks

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -13,11 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chad 1.0',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Chad 1.0'),
     );
   }
 }
@@ -48,21 +48,33 @@ void _onButtonClicked(BuildContext context) {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        leading: Icon(Icons.menu),
+        
       ),
-      body: Center(
-        child: Column(
+
+      body: Stack(
+      children: [
+        Image.asset(
+          'assets/img/palms.jpg',
+          height: double.infinity,
+          width: double.infinity,
+          fit: BoxFit.cover,
+        ),
+       Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('Ask me anything!'),
             Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: EdgeInsets.all(16.0),
+
               child: TextField(
                 controller: _textController,
-                decoration: const InputDecoration(
+                decoration: InputDecoration(
                   border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Colors.white.withValues(alpha: 0.4),
                   labelText: 'Your input'
                 ),
               ), 
@@ -80,6 +92,7 @@ void _onButtonClicked(BuildContext context) {
             )
           ],
         ),
+      ],
       ),
     );
   }
